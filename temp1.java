76cbe8339b06294c7a2b07f29ef59ec76350ec66
Andreas Bourakis <bourakis@gmail.com>
2020-01-13 12:48:20 +0200

    Create temp1.java

---
+++ temp1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Test
+{
+  public static void main(String[] args)
+  {
+    Scanner temp = new Scanner(System.in);
+    System.out.print("Enter temperature: ");
+
+    int get_temp = Integer.parseInt(temp.nextLine());
+
+    if(get_temp < 36)
+    {
+      System.out.println("Eisai ok!");
+    }
+    else
+    {
+      System.out.println("Houston we got a problem!");
+    }
+  }
+}
