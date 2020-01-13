8d2d83d1afbb094feb063036ffd0a67255ade10b
Andreas Bourakis <bourakis@gmail.com>
2020-01-13 13:00:25 +0200

    Create Temp3.java

---
+++ Temp3.java
@@ -0,0 +1,31 @@
+package getTempFloat;
+import java.util.Scanner;
+
+public class getTempFloat 
+{
+  public static void main(String[] args) 
+  {	
+    Scanner temp = new Scanner(System.in); 
+    System.out.println("Enter temperature");
+
+    float get_temp = Float.parseFloat(temp.nextLine());
+
+    if(get_temp <= 35.5)
+    {
+      System.out.println("Exeis ypothermia!");
+    }
+    else if(get_temp <= 36.6 || get_temp <= 36.8) 
+    {
+      System.out.println("Eisai ok!");
+    }
+    else if(get_temp >= 36.8 && get_temp <= 40)
+    {
+      System.out.println("Exeiw pyreto!");
+    }
+    else if(get_temp > 40)
+    {
+      System.out.println("Pethaineis!");
+    }
+
+  }	
+}
