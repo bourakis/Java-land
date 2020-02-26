77acf6c999cf37d696f00cc307e579d34eec086e
Andreas Bourakis <bourakis@gmail.com>
2020-02-26 10:19:20 +0200

    Update Bycicle.java

--- Object Oriented Programming/Examples/Bycicle/Bycicle.java
+++ Object Oriented Programming/Examples/Bycicle/Bycicle.java
@@ -7,28 +7,28 @@ public class Bicycle
 
     void changeCadence(int newValue) 
     {
-         cadence = newValue;
+        cadence = newValue;
     }
 
     void changeGear(int newValue) 
     {
-         gear = newValue;
+        gear = newValue;
     }
 
     void speedUp(int increment) 
     {
-         speed = speed + increment;   
+        speed = speed + increment;   
     }
 
     void applyBrakes(int decrement) 
     {
-         speed = speed - decrement;
+        speed = speed - decrement;
     }
 
     void printStates() 
     {
-         System.out.println("cadence:" +
-             cadence + " speed:" + 
-             speed + " gear:" + gear);
+        System.out.println("cadence:" +
+        cadence + " speed:" + 
+        speed + " gear:" + gear);
     }
 }
