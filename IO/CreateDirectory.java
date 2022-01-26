a581737124f77bd3a6f604d3d1d4428f92248468
Andreas Bourakis <bourakis@gmail.com>
2022-01-26 20:25:19 +0200

    Update CreateDirectory.java

--- IO/CreateDirectory.java
+++ IO/CreateDirectory.java
@@ -1,19 +1,29 @@
 import java.io.File;
 import java.util.Scanner;
-public class CreateDirectory {
-   public static void main(String args[]) {
+public class CreateDirectory 
+{
+   public static void main(String args[]) 
+   {
       System.out.println("Enter the path to create a directory: ");
       Scanner sc = new Scanner(System.in);
+      
       String path = sc.next();
+      
       System.out.println("Enter the name of the desired a directory: ");
       path = path+sc.next();
+      
       //Creating a File object
       File file = new File(path);
+      
       //Creating the directory
       boolean bool = file.mkdir();
-      if(bool){
+      
+      if(bool)
+      {
          System.out.println("Directory created successfully");
-      }else{
+      }
+      else
+      {
          System.out.println("Sorry couldn’t create specified directory");
       }
    }
