73ac84d6909e924c74308b8cc16eb966d3b07b14
Andreas Bourakis <bourakis@gmail.com>
2022-01-26 20:26:31 +0200

    Update DeleteFile.java

--- IO/DeleteFile.java
+++ IO/DeleteFile.java
@@ -1,11 +1,17 @@
 import java.io.File;  // Import the File class
 
-public class DeleteFile {
-  public static void main(String[] args) { 
+public class DeleteFile 
+{
+  public static void main(String[] args) 
+  { 
     File myObj = new File("filename.txt"); 
-    if (myObj.delete()) { 
+    
+    if (myObj.delete())
+    { 
       System.out.println("Deleted the file: " + myObj.getName());
-    } else {
+    } 
+    else 
+    {
       System.out.println("Failed to delete the file.");
     } 
   } 
