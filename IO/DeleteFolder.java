beb70a8f07f24185de8d2fbe5ab24925422f209c
Andreas Bourakis <bourakis@gmail.com>
2022-01-26 20:27:03 +0200

    Update DeleteFolder.java

--- IO/DeleteFolder.java
+++ IO/DeleteFolder.java
@@ -1,11 +1,17 @@
 import java.io.File; 
 
-public class DeleteFolder {
-  public static void main(String[] args) { 
+public class DeleteFolder 
+{
+  public static void main(String[] args) 
+  { 
     File myObj = new File("C:\\Users\\MyName\\Test"); 
-    if (myObj.delete()) { 
+    
+    if (myObj.delete()) 
+    { 
       System.out.println("Deleted the folder: " + myObj.getName());
-    } else {
+    } 
+    else 
+    {
       System.out.println("Failed to delete the folder.");
     } 
   } 
