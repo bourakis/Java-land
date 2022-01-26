4fdcc6c6669d63f00aa283dfcb0a283bdb167be8
Andreas Bourakis <bourakis@gmail.com>
2022-01-26 20:26:05 +0200

    Update CreateFile.java

--- IO/CreateFile.java
+++ IO/CreateFile.java
@@ -1,16 +1,25 @@
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 
-public class CreateFile {
-  public static void main(String[] args) {
-    try {
+public class CreateFile 
+{
+  public static void main(String[] args) 
+  {
+    try 
+    {
       File myObj = new File("filename.txt");
-      if (myObj.createNewFile()) {
+      
+      if (myObj.createNewFile()) 
+      {
         System.out.println("File created: " + myObj.getName());
-      } else {
+      } 
+      else 
+      {
         System.out.println("File already exists.");
       }
-    } catch (IOException e) {
+    } 
+    catch (IOException e) 
+    {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
